b7efc1c9e2d6d6994a50455d531e901d087fbaee
AraceliPac <a23arapacmun@inspedralbes.cat>
2025-03-15 18:44:51 +0100

    add npm run build

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,18 +1,31 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt","pinia-plugin-persistedstate/nuxt", "@nuxtjs/tailwindcss", "@nuxt/image", "@nuxt/icon"],
+  modules: [
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@nuxt/icon",
+  ],
   devtools: { enabled: true },
+
+  // Configuración SSR global
   ssr: true,
+
+  routeRules: {
+    "/home": { ssr: true }, //  SSR
+    "/**": { ssr: false }, //  (SPA)
+  },
+
   vite: {
     define: {
-      global: 'globalThis'
+      global: "globalThis",
     },
     resolve: {
       alias: {
-        stream: 'stream-browserify'
-      }
-    }
-  }
+        stream: "stream-browserify",
+      },
+    },
+  },
 });
